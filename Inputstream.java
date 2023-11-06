5caaafd475204c58f19baff98199b5a6105641d8
BharathrajRB <bharathrajrb20it@gmail.com>
2023-11-06 11:54:59 +0530

    latest

--- Inputstream.java
+++ Inputstream.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 
 public class Inputstream {
     public static void main(String[] args) {
-        try (InputStream inputStream = new FileInputStream("example.txt")) {
+        try {
+            InputStream inputStream = new FileInputStream("example.txt");
             int data = inputStream.read();
             while (data != -1) {
                 System.out.print((char) data);
                 data = inputStream.read();
             }
+            inputStream.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
