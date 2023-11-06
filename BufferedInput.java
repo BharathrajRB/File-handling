7adced844d47dfc03015ca86cb87db693f253d9b
BharathrajRB <bharathrajrb20it@gmail.com>
2023-11-06 11:02:55 +0530

    File handling

---
+++ BufferedInput.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class BufferedInput {
+    public static void main(String[] args) {
+        try (InputStream inputStream = new FileInputStream("example.txt");
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream)) {
+            int data = bufferedInputStream.read();
+            while (data != -1) {
+                System.out.print((char) data);
+                data = bufferedInputStream.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
